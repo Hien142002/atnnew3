f293e785257f3c948cea5a1d9442dac018b3f324
Vohuyhau <hauvhgcc200224@fpt.edu.vn>
2023-06-21 04:02:16 +0700

    huhu

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php 
-    // $conn = mysqli_connect('localhost','root','','atndb') 
-    // or die("Cannot connect database".mysqli_connect_error());
-
-    $conn = mysqli_connect('ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','zzfe1hh4bgsacqtt','blvlyjxlvu66rf2g','bdyhffqfufbkwowv') 
+    $conn = mysqli_connect('localhost','root','','atndb') 
     or die("Cannot connect database".mysqli_connect_error());
+
+    // $conn = mysqli_connect('ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','zzfe1hh4bgsacqtt','blvlyjxlvu66rf2g','bdyhffqfufbkwowv') 
+    // or die("Cannot connect database".mysqli_connect_error());
 
 ?>
