3f68154c6bd28ca6824c2746f9827b5e2b105f93
AlgerHowrd <hienhngcc200138@fpt.edu.vn>
2023-12-16 18:53:58 +0700

    123

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     // $conn = mysqli_connect('localhost','root','','atndb') 
     // or die("Cannot connect database".mysqli_connect_error());
 
-    $conn = mysqli_connect('t07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','wt0jzt02aw8b62pe','xakbb9cv68gv3sjr','mbzjkz00pdki7iri') 
+    $conn = mysqli_connect('t07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','k9z34n0jxv41yicj','vq13836m6nz4k1ux','vq13836m6nz4k1ux') 
     or die("Cannot connect database".mysqli_connect_error());
 
 ?>
